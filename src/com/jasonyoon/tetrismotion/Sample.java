09b0cac19f0668c0aed4707a22a5d74d84f523cc
jasony0206 <jasony0206@gmail.com>
2015-10-03 03:52:07 -0700

    Extracted Listener out of Sample

--- src/com/jasonyoon/tetrismotion/Sample.java
+++ src/com/jasonyoon/tetrismotion/Sample.java
@@ -27,13 +27,3 @@ public class Sample {
     }
 }
 
-class SampleListener extends Listener {
-
-    public void onConnect(Controller controller) {
-        System.out.println("Connected");
-    }
-
-    public void onFrame(Controller controller) {
-        System.out.println("Frame available");
-    }
-}
---
+++ src/com/jasonyoon/tetrismotion/SampleListener.java
@@ -0,0 +1,16 @@
+package com.jasonyoon.tetrismotion;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Listener;
+
+public class SampleListener extends Listener {
+
+    public void onConnect(Controller controller) {
+        System.out.println("Connected");
+    }
+
+    public void onFrame(Controller controller) {
+        System.out.println("Frame available");
+    }
+}
+
